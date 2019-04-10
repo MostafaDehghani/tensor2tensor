75f542675a28d7dc050356f943117aa302fb4c3c
MostafaDehghani <mdehgha1@ilps-h0.science.uva.nl>
2019-04-10 12:51:03 +0200

    started adding gumbel softmax codes

--- tensor2tensor/models/research/universal_transformer_util.py
+++ tensor2tensor/models/research/universal_transformer_util.py
@@ -1643,3 +1643,46 @@ def add_step_timing_signal(x, step, hparams):
     x_with_timing = tf.concat((x, signal_tiled), axis=-1)
 
   return x_with_timing
+
+
+
+def sample_gumbel(shape, eps=1e-20):
+  """Sample from Gumbel(0, 1)"""
+  U = tf.random_uniform(shape,minval=0,maxval=1)
+  return -tf.log(-tf.log(U + eps) + eps)
+
+def gumbel_softmax_sample(logits, temperature):
+  """ Draw a sample from the Gumbel-Softmax distribution"""
+  y = logits + sample_gumbel(tf.shape(logits))
+  return tf.nn.softmax( y / temperature)
+
+
+straight_through=False # if True, use Straight-through Gumbel-Softmax
+tau0 = 5.0
+learn_temp = True # if learn the softmax temperature
+
+def gumbel_softmax(logits, temperature, straight_through=False):
+  """Sample from the Gumbel-Softmax distribution and optionally discretize.
+  Args:
+    logits: [batch_size, n_class] unnormalized log-probs
+    temperature: non-negative scalar
+    hard: if True, take argmax, but differentiate w.r.t. soft sample y
+  Returns:
+    [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+    If hard=True, then the returned sample will be one-hot, otherwise it will
+    be a probabilitiy distribution that sums to 1 across classes
+  """
+  y = gumbel_softmax_sample(logits, temperature)
+  if straight_through:
+    k = tf.shape(logits)[-1]
+    #y_hard = tf.cast(tf.one_hot(tf.argmax(y,1),k), y.dtype)
+    y_hard = tf.cast(tf.equal(y,tf.reduce_max(y,1,keep_dims=True)),y.dtype)
+    y = tf.stop_gradient(y_hard - y) + y
+  return y
+
+
+
+# temperature
+tau = tf.Variable(tau0,name="temperature",trainable=learn_temp)
+# sample. (set hard=True for ST Gumbel-Softmax)
+y = gumbel_softmax(logits_y,tau,hard=False)
